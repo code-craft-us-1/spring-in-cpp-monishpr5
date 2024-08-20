d2dbc610840fb6d223cdf6d510adf9ef21293b53
monishpr5 <107621862+monishpr5@users.noreply.github.com>
2024-08-20 14:09:32 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,6 +2,11 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
+    struct Stats {
+        int min;
+        int max;
+        double average;
+    };
     
     Stats ComputeStatistics(const std::vector<double>& );
 }
